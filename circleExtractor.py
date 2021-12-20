0298f1a132b089c6e8a71b542bea9f5867516fdd
anthonyweidai <davidietop@163.com>
2021-12-20 09:40:23 +0800

    fix bugs skip detecting

--- circleExtractor.py
+++ circleExtractor.py
@@ -2,49 +2,59 @@ import os
 from pathlib import Path
 from glob import glob
 import cv2
+from tqdm import tqdm
 import numpy as np
 
 
+WriteMode = 1
 DatasetPath = r'D:\dataset\Skin Disease\ISIC2019\data'
 ClassNames = os.listdir(DatasetPath)
 
 for ClassName in ClassNames:
     ImgsPath = glob(DatasetPath + '/%s/*' % (ClassName))
-    for ImgPath in ImgsPath:
-        # ImgsPath = './dataset/ISIC2019/bcc/ISIC_0053762.jpg'
-        Img = cv2.imread(ImgPath) # ISIC_0053762.jpg, ISIC_0053506.jpg
-        # cv2.imshow('The original image', Img)
-        # cv2.waitKey(0)
-
-        GrayImg = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)
-        # cv2.imshow('Gray image ', GrayImg)
-        # cv2.waitKey(0)
-
-        _, Thred = cv2.threshold(GrayImg, 50, 255, cv2.THRESH_BINARY)
-        # cv2.imshow('Thred', Thred)
-        # cv2.waitKey(0)
-
-        kernel = np.ones((2,2),np.uint8)
-        OpeningEdges = cv2.morphologyEx(Thred, cv2.MORPH_CLOSE, kernel, iterations=2)
-        # cv2.imshow('Opening Edges', OpeningEdges)
-        # cv2.waitKey(0)
-
-        Contours = cv2.findContours(OpeningEdges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        Contour = Contours[0][0]
-        Area = cv2.contourArea(Contour)
-
-        if Area / GrayImg.size < 0.9 and Area / GrayImg.size > 0.01:
-            x, y, w, h = cv2.boundingRect(Contour)
-            CropImg = Img[y : y + h, x : x + w]
-            # cv2.imshow('Cropped Eye', CropImg)
-            # cv2.waitKey(0)
-
-            # # Code to close Window
-            # cv2.destroyAllWindows()
+    with tqdm(total=len(ImgsPath), colour='blue', ncols=100) as t:
+        for ImgPath in ImgsPath:
+            # ImgPath = './dataset/ISIC2019/ack/ISIC_0060404.jpg'
+            Img = cv2.imread(ImgPath) # ISIC_0053762.jpg, ISIC_0053506.jpg
             
-            Head, Tail = os.path.split(ImgPath)
-            DestPath = r'D:\dataset\Skin Disease\ISIC2019' + '/crop_img/' + ClassName
-            Path(DestPath).mkdir(parents=True, exist_ok=True)
-            DestPath = DestPath + '/' + Tail
+            GrayImg = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)
             
-            cv2.imwrite(DestPath, CropImg)
+            _, Thred = cv2.threshold(GrayImg, 50, 255, cv2.THRESH_BINARY)
+            
+            Kernal = np.ones((2,2),np.uint8)
+            OpeningEdges = cv2.morphologyEx(Thred, cv2.MORPH_CLOSE, Kernal, iterations=2)
+            
+            Contours = cv2.findContours(OpeningEdges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            Area = 0
+            Contour = None
+            for i in range(len(Contours[0])):
+                ContourTemp = Contours[0][i]
+                AreaTemp = cv2.contourArea(ContourTemp)
+                if Area < AreaTemp:
+                    Area = AreaTemp
+                    Contour = ContourTemp
+            
+            if WriteMode == 0:
+                cv2.imshow('The original image', Img)
+                cv2.imshow('Gray image ', GrayImg)
+                cv2.imshow('Thred', Thred)
+                cv2.imshow('Opening Edges', OpeningEdges)
+                cv2.waitKey(0)
+                
+                cv2.destroyAllWindows() # Code to close Window
+
+            if Area / GrayImg.size < 0.9 and Area / GrayImg.size > 0.01:
+                x, y, w, h = cv2.boundingRect(Contour)
+                CropImg = Img[y : y + h, x : x + w]
+                # cv2.imshow('Cropped Img', CropImg)
+                # cv2.waitKey(0)
+
+                if WriteMode == 1:
+                    Head, Tail = os.path.split(ImgPath)
+                    DestPath = r'D:\dataset\Skin Disease\ISIC2019' + '/crop_img/' + ClassName
+                    Path(DestPath).mkdir(parents=True, exist_ok=True)
+                    DestPath = DestPath + '/' + Tail
+                    
+                    cv2.imwrite(DestPath, CropImg)
+                    
+            t.update()
